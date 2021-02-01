6b9966ddb4995a0b6fd82c45a9605d86fc1bc38f
Chudier <chudierp@gmail.com>
2021-02-01 07:26:04 -0700

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'CHUDIER',
+      headerLinks: [
+          {title: 'home', path:'/'},
+          {title: 'contact', path:'/contact'},
+      ],
+      home: {
+
+      }  
+    }
+  }
+
+
+  render() {
+    return (
+      <Router>
+        <Container className='p-0' fluid={true}>
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
+            <Navbar.Brand className='display-1 font-weight-bolder'>CHUDIER</Navbar.Brand>
+            <Navbar.Toggle aria-controls='navbar-toggle' />
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className='ml-auto'>
+                <Link className='nav-link' to='/'>home</Link>
+                <Link className='nav-link' to='/contact'>contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    )
+  }
+  
+    
 }
 
 export default App;
